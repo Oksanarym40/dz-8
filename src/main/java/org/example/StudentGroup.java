f615e0878eb9427fab53a22735d0a8e53f1b6306
Oksanarym40 <135235310+Oksanarym40@users.noreply.github.com>
2023-07-19 15:49:40 -0400

    Update StudentGroup.java
    Changed to completedTasks

--- src/main/java/org/example/StudentGroup.java
+++ src/main/java/org/example/StudentGroup.java
@@ -7,65 +7,70 @@ package org.example;
         import java.util.Map;
 
     public abstract class StudentGroup {
-        private Student mayor;
-        private List<Student> students;
-        private List<String> tasks;
-        private Map<Integer, Boolean> taskStatus;
+    private Student mayor;
+    private List<Student> students;
+    private List<String> tasks;
+    private Map<Student, List<String>> completedTasks;
 
-        public StudentGroup(Student mayor) {
-            this.mayor = mayor;
-            this.students = new ArrayList<>();
-            this.tasks = new ArrayList<>();
-            this.taskStatus = new HashMap<>();
-        }
+    public StudentGroup(Student mayor) {
+        this.mayor = mayor;
+        this.students = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        this.completedTasks = new HashMap<>();
+    }
 
-        public Student getMayor() {
-            return mayor;
-        }
+    public Student getMayor() {
+        return mayor;
+    }
 
-        public void changeMayor(Student newMayor) {
-            this.mayor = newMayor;
-        }
+    public void changeMayor(Student newMayor) {
+        this.mayor = newMayor;
+    }
 
-        public List<Student> getStudents() {
-            return new ArrayList<>(students);
-        }
+    public List<Student> getStudents() {
+        return new ArrayList<>(students);
+    }
 
-        public void addStudent(Student student) {
-            students.add(student);
-        }
+    public void addStudent(Student student) {
+        students.add(student);
+    }
 
-        public void deleteStudent(Student student) {
-            students.remove(student);
-        }
+    public void deleteStudent(Student student) {
+        students.remove(student);
+        completedTasks.remove(student);
+    }
 
-        public List<String> getTasks() {
-            return new ArrayList<>(tasks);
-        }
+    public List<String> getTasks() {
+        return new ArrayList<>(tasks);
+    }
 
-        public void addTask(String task) {
-            tasks.add(task);
-            for (Student student : students) {
-                taskStatus.put(student.getStudentId(), false);
-            }
-        }
-
-        public void markTaskAsCompleted(Student student, String task) {
-            if (students.contains(student) && tasks.contains(task)) {
-                taskStatus.put(student.getStudentId(), true);
-            }
-        }
-
-
-        protected abstract void customMethod();
-
-        @Override
-        public String toString() {
-            return "StudentsGroup{" +
-                    "mayor=" + mayor +
-                    ", students=" + students +
-                    ", tasks=" + tasks +
-                    '}';
+    public void addTask(String task) {
+        tasks.add(task);
+        for (Student student : students) {
+            completedTasks.put(student, new ArrayList<>());
         }
     }
 
+    public void markTaskAsCompleted(Student student, String task) {
+        if (students.contains(student) && tasks.contains(task)) {
+            List<String> studentCompletedTasks = completedTasks.get(student);
+            studentCompletedTasks.add(task);
+            completedTasks.put(student, studentCompletedTasks);
+        }
+    }
+
+    public Map<Student, List<String>> getCompletedTasks() {
+        return new HashMap<>(completedTasks);
+    }
+
+    protected abstract void customMethod();
+
+    @Override
+    public String toString() {
+        return "StudentsGroup{" +
+                "mayor=" + mayor +
+                ", students=" + students +
+                ", tasks=" + tasks +
+                '}';
+    }
+}
